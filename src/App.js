2cd97e5f5ce08836b5da9c5409f6da7f5008469a
Josh Wilson <joshua.wilson6289@gmail.com>
2023-03-27 15:29:40 -0500

    initial commit of react app to test

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import BookCreate from './BookCreate';
+import BookList from './BookList';
+
+function App() {
+
+  const [books, setBooks] = useState([])
+
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      {id: Math.round(Math.random() * 9999), title}
+    ];
+    setBooks(updatedBooks);
+  }
+
+  const editBookById = (id, title) => {
+    
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return {...book, title};
+      }
+
+      return book;
+    })
+    setBooks(updatedBooks)
+  }
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    })
+    setBooks(updatedBooks)
+  }
+
+  return (
+  <div className="app">
+    <h1>Reading List</h1>
+    <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
+    <BookCreate onCreate={createBook} />
+  </div>
+  );
+}
+
+export default App;
